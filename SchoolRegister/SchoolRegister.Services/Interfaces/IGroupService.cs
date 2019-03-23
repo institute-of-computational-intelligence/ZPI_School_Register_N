969af8fcd4edaf3751d422031629e29a00811f12
BlaszczykB <bartlomiej.blaszczik@gmail.com>
2019-03-23 15:28:19 +0100

    Lab3 - exercise - initial

---
+++ SchoolRegister/SchoolRegister.Services/Interfaces/IGroupService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.Services.Interfaces
+{
+    public interface IGroupService
+    {
+    }
+}
---
+++ SchoolRegister/SchoolRegister.Services/Services/GradeService.cs
@@ -0,0 +1,16 @@
+﻿using SchoolRegister.DAL.EF;
+using SchoolRegister.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.Services.Services
+{
+    public class GradeService: BaseService, IGradeService
+    {
+        public GradeService(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
